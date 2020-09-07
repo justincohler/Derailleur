e3ad636ea537597acf991607c0d83361d9c8c426
Justin Cohler <justincohler@gmail.com>
2020-09-07 07:55:46 -0400

    Format buttons

--- Derailleur/UI/DerailleurMainView.m
+++ Derailleur/UI/DerailleurMainView.m
@@ -35,25 +35,25 @@
 
 - (instancetype) init
 {
-	self = [super init];
-	if (self) {
-		[self setupViews];
-		[self setupLayout];
-	}
-	return self;
+    self = [super init];
+    if (self) {
+        [self setupViews];
+        [self setupLayout];
+    }
+    return self;
 }
 
 - (void) setupViews
 {
-	[self setWantsLayer:YES];
-	[self.layer setBackgroundColor:[[NSColor darkGrey] CGColor]];
+    [self setWantsLayer:YES];
+    [self.layer setBackgroundColor:[[NSColor darkGrey] CGColor]];
     [self setupMetrics];
     [self setupStatusBar];
     [self setupButtons];
 }
 
 - (IBAction) handleStartRecordingEvent : (id) sender  {
-   NSLog(@"Starting recording activity");
+    NSLog(@"Starting recording activity");
     _startRecordingButton.hidden = true;
     _pauseRecordingButton.hidden = false;
 }
@@ -67,8 +67,8 @@
 }
 
 - (IBAction) handleCancelRecordingEvent : (id) sender {
-   NSAlert *alert = [[NSAlert alloc] init];
-    [alert addButtonWithTitle:@"Continue"];
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert addButtonWithTitle:@"Discard"];
     [alert addButtonWithTitle:@"Cancel"];
     [alert setMessageText:@"Alert"];
     [alert setInformativeText:@"NSWarningAlertStyle \r Are you sure you want to discard this ride?"];
@@ -85,11 +85,15 @@
 
 - (void)alertDidEnd:(NSAlert *)alert returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo
 {
- if (returnCode == NSAlertFirstButtonReturn) {
-     NSLog(@"Discarded ride");
- } else {
-     NSLog(@"Cancelled discard event");
- }
+    if (returnCode == NSAlertFirstButtonReturn) {
+        NSLog(@"Discarded ride");
+        _cancelRecordingButton.hidden = true;
+        _resumeRecordingButton.hidden = true;
+        _finishAndSaveRecordingButton.hidden = true;
+        _startRecordingButton.hidden = false;
+    } else {
+        NSLog(@"Cancelled discard event");
+    }
 }
 
 - (void) setupMetrics
@@ -117,14 +121,14 @@
 {
     /* Set up cadence view and associated subviews */
     _cadenceView = [[NSView alloc] init];
-
+    
     [_cadenceView setWantsLayer:YES];
     [[_cadenceView layer] setCornerRadius:10];
     [[_cadenceView layer] setBackgroundColor: [[NSColor lightGrey] CGColor]];
     [_cadenceView setTranslatesAutoresizingMaskIntoConstraints:NO];
-       
+    
     NSTextField *cadenceTitle = [[NSTextField alloc] init];
-
+    
     [cadenceTitle setEditable:NO];
     [cadenceTitle setBezeled:NO];
     [cadenceTitle setDrawsBackground:NO];
@@ -132,14 +136,14 @@
     [cadenceTitle setAlignment:NSTextAlignmentCenter];
     [cadenceTitle setFont:[NSFont boldSystemFontOfSize:18]];
     [cadenceTitle setStringValue:@"Cadence (RPM)"];
-
+    
     [_cadenceView addSubview:cadenceTitle];
     [cadenceTitle centerHorizontallyInSuperview];
     [cadenceTitle.bottomAnchor constraintEqualToAnchor:_cadenceView.bottomAnchor constant: -20].active = YES;
-
+    
     /* Set up cadence label */
     _cadenceLabel = [[NSTextField alloc] init];
-
+    
     [_cadenceLabel setEditable:NO];
     [_cadenceLabel setBezeled:NO];
     [_cadenceLabel setDrawsBackground:NO];
@@ -147,7 +151,7 @@
     [_cadenceLabel setStringValue:@"0"];
     [_cadenceLabel setAlignment:NSTextAlignmentCenter];
     [_cadenceLabel setFont: [NSFont boldSystemFontOfSize:75]];
-
+    
     [_cadenceView addSubview:_cadenceLabel];
 }
 
@@ -155,14 +159,14 @@
 {
     /* Set up resistance view and associated subviews */
     _resistanceView = [[NSView alloc] init];
-
+    
     [_resistanceView setWantsLayer:YES];
     [[_resistanceView layer] setCornerRadius:10];
     [[_resistanceView layer] setBackgroundColor: [[NSColor lightGrey] CGColor]];
     [_resistanceView setTranslatesAutoresizingMaskIntoConstraints:NO];
-
+    
     NSTextField *resistanceTitle = [[NSTextField alloc] init];
-
+    
     [resistanceTitle setEditable:NO];
     [resistanceTitle setBezeled:NO];
     [resistanceTitle setDrawsBackground:NO];
@@ -170,14 +174,14 @@
     [resistanceTitle setAlignment:NSTextAlignmentCenter];
     [resistanceTitle setFont:[NSFont boldSystemFontOfSize:18]];
     [resistanceTitle setStringValue:@"Resistance"];
-
+    
     [_resistanceView addSubview:resistanceTitle];
     [resistanceTitle centerHorizontallyInSuperview];
     [resistanceTitle.bottomAnchor constraintEqualToAnchor:_resistanceView.bottomAnchor constant: -20].active = YES;
-
+    
     /* Set up resistance label */
     _resistanceLabel = [[NSTextField alloc] init];
-
+    
     [_resistanceLabel setEditable:NO];
     [_resistanceLabel setBezeled:NO];
     [_resistanceLabel setDrawsBackground:NO];
@@ -185,7 +189,7 @@
     [_resistanceLabel setStringValue:@"0%"];
     [_resistanceLabel setAlignment:NSTextAlignmentCenter];
     [_resistanceLabel setFont: [NSFont boldSystemFontOfSize:75]];
-
+    
     [_resistanceView addSubview:_resistanceLabel];
 }
 
@@ -220,118 +224,128 @@
 
 - (void) setupButtons
 {
-   _startRecordingButton = [[NSButton alloc] init];
-   [_startRecordingButton setTitle:@"Start Recording"];
-   [_startRecordingButton setAction:@selector(handleStartRecordingEvent:)];
-   [_startRecordingButton setTarget:self];
-   [_startRecordingButton setHidden:false];
-   
-   _pauseRecordingButton = [[NSButton alloc] init];
-   [_pauseRecordingButton setTitle:@"Pause"];
-   [_pauseRecordingButton setAction:@selector(handlePauseRecordingEvent:)];
-   [_pauseRecordingButton setTarget:self];
-   [_pauseRecordingButton setHidden:true];
-   
-   _cancelRecordingButton = [[NSButton alloc] init];
-   [_cancelRecordingButton setTitle:@"Discard"];
-   [_cancelRecordingButton setAction:@selector(handleCancelRecordingEvent:)];
-   [_cancelRecordingButton setTarget:self];
-   [_cancelRecordingButton setHidden:true];
-   
-   _resumeRecordingButton = [[NSButton alloc] init];
-   [_resumeRecordingButton setTitle:@"Resume"];
-   [_resumeRecordingButton setAction:@selector(handleResumeRecordingEvent:)];
-   [_resumeRecordingButton setTarget:self];
-   [_resumeRecordingButton setHidden:true];
-   
-   _finishAndSaveRecordingButton = [[NSButton alloc] init];
-   [_finishAndSaveRecordingButton setTitle:@"Finish and Save"];
-   [_finishAndSaveRecordingButton setAction:@selector(handleFinishAndSaveRecordingEvent:)];
-   [_finishAndSaveRecordingButton setTarget:self];
-   [_finishAndSaveRecordingButton setHidden:true];
-   
-   _buttonsStack = [[NSStackView alloc] init];
-   
-   [_buttonsStack setOrientation:NSUserInterfaceLayoutOrientationHorizontal];
-   [_buttonsStack setAlignment:NSLayoutAttributeCenterY];
-   [_buttonsStack setDistribution:NSStackViewDistributionFillEqually];
-   [_buttonsStack setTranslatesAutoresizingMaskIntoConstraints:NO];
-   [_buttonsStack setSpacing:15];
-   
-   [_buttonsStack addArrangedSubview:_startRecordingButton];
-   [_buttonsStack addArrangedSubview:_pauseRecordingButton];
-   [_buttonsStack addArrangedSubview:_cancelRecordingButton];
-   [_buttonsStack addArrangedSubview:_resumeRecordingButton];
-   [_buttonsStack addArrangedSubview:_finishAndSaveRecordingButton];
+    _startRecordingButton = [[NSButton alloc] init];
+    [_startRecordingButton setTitle:@"Start Recording"];
+    [_startRecordingButton setAction:@selector(handleStartRecordingEvent:)];
+    [_startRecordingButton setBezelStyle:NSBezelStyleRounded];
+    [_startRecordingButton setTarget:self];
+    [_startRecordingButton setHidden:false];
     
-   [self addSubview:_buttonsStack];
+    _pauseRecordingButton = [[NSButton alloc] init];
+    [_pauseRecordingButton setTitle:@"Pause"];
+    [_pauseRecordingButton setBezelStyle:NSBezelStyleRounded];
+    [_pauseRecordingButton setAction:@selector(handlePauseRecordingEvent:)];
+    [_pauseRecordingButton setTarget:self];
+    [_pauseRecordingButton setHidden:true];
+    
+    _cancelRecordingButton = [[NSButton alloc] init];
+    [_cancelRecordingButton setTitle:@"Discard"];
+    [_cancelRecordingButton setBezelStyle:NSBezelStyleRounded];
+    [_cancelRecordingButton setAction:@selector(handleCancelRecordingEvent:)];
+    [_cancelRecordingButton setTarget:self];
+    [_cancelRecordingButton setHidden:true];
+    
+    _resumeRecordingButton = [[NSButton alloc] init];
+    [_resumeRecordingButton setTitle:@"Resume"];
+    [_resumeRecordingButton setBezelStyle:NSBezelStyleRounded];
+    [_resumeRecordingButton setAction:@selector(handleResumeRecordingEvent:)];
+    [_resumeRecordingButton setTarget:self];
+    [_resumeRecordingButton setHidden:true];
+    
+    _finishAndSaveRecordingButton = [[NSButton alloc] init];
+    [_finishAndSaveRecordingButton setTitle:@"Finish and Save"];
+    [_finishAndSaveRecordingButton setBezelStyle:NSBezelStyleRounded];
+    [_finishAndSaveRecordingButton setAction:@selector(handleFinishAndSaveRecordingEvent:)];
+    [_finishAndSaveRecordingButton setTarget:self];
+    [_finishAndSaveRecordingButton setHidden:true];
+    
+    _buttonsStack = [[NSStackView alloc] init];
+    
+    [_buttonsStack setOrientation:NSUserInterfaceLayoutOrientationHorizontal];
+    [_buttonsStack setAlignment:NSLayoutAttributeCenterY];
+    [_buttonsStack setDistribution:NSStackViewDistributionFillEqually];
+    [_buttonsStack setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [_buttonsStack setSpacing:15];
+    
+    [_buttonsStack addArrangedSubview:_startRecordingButton];
+    [_buttonsStack addArrangedSubview:_pauseRecordingButton];
+    [_buttonsStack addArrangedSubview:_cancelRecordingButton];
+    [_buttonsStack addArrangedSubview:_resumeRecordingButton];
+    [_buttonsStack addArrangedSubview:_finishAndSaveRecordingButton];
+    
+    [self addSubview:_buttonsStack];
 }
 
 - (void) setupLayout
 {
-	[_cadenceLabel centerInSuperviewAdjustedVertically:-15];
-	[_resistanceLabel centerInSuperviewAdjustedVertically:-15];
-	
-	[NSLayoutConstraint activateConstraints:@[
-		[_statusBar.heightAnchor constraintEqualToConstant:25],
-		[_statusBar.bottomAnchor constraintEqualToAnchor:self.bottomAnchor constant:-20],
-		[_statusBar.leftAnchor constraintEqualToAnchor:self.leftAnchor constant:20],
-		[_statusBar.rightAnchor constraintEqualToAnchor:self.rightAnchor constant:-20],
-		
-		[_statusDot.heightAnchor constraintEqualToConstant:10],
-		[_statusDot.widthAnchor constraintEqualToConstant:10],
-		[_statusDot.centerYAnchor constraintEqualToAnchor:_statusBar.centerYAnchor],
-		[_statusDot.rightAnchor constraintEqualToAnchor:_statusBar.rightAnchor constant:-10],
-		
-		[_statusLabel.leftAnchor constraintEqualToAnchor:_statusBar.leftAnchor constant:10],
-		[_statusLabel.centerYAnchor constraintEqualToAnchor:_statusBar.centerYAnchor],
-		
-		[_metricsStack.topAnchor constraintEqualToAnchor:self.topAnchor constant:30],
-		[_metricsStack.bottomAnchor constraintEqualToAnchor:_statusBar.topAnchor constant:-15],
-		[_metricsStack.leftAnchor constraintEqualToAnchor:self.leftAnchor constant:20],
-		[_metricsStack.rightAnchor constraintEqualToAnchor:self.rightAnchor constant:-20]
-	]];
+    [_cadenceLabel centerInSuperviewAdjustedVertically:-15];
+    [_resistanceLabel centerInSuperviewAdjustedVertically:-15];
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [_buttonsStack.heightAnchor constraintEqualToConstant:30],
+        [_buttonsStack.bottomAnchor constraintEqualToAnchor:self.bottomAnchor constant:-20],
+        [_buttonsStack.leftAnchor constraintEqualToAnchor:self.leftAnchor constant:20],
+        [_buttonsStack.rightAnchor constraintEqualToAnchor:self.rightAnchor constant:-20],
+        
+        [_statusBar.heightAnchor constraintEqualToConstant:25],
+        [_statusBar.bottomAnchor constraintEqualToAnchor:_buttonsStack.topAnchor constant:-20],
+        [_statusBar.leftAnchor constraintEqualToAnchor:self.leftAnchor constant:20],
+        [_statusBar.rightAnchor constraintEqualToAnchor:self.rightAnchor constant:-20],
+        
+        [_statusDot.heightAnchor constraintEqualToConstant:10],
+        [_statusDot.widthAnchor constraintEqualToConstant:10],
+        [_statusDot.centerYAnchor constraintEqualToAnchor:_statusBar.centerYAnchor],
+        [_statusDot.rightAnchor constraintEqualToAnchor:_statusBar.rightAnchor constant:-10],
+        
+        [_statusLabel.leftAnchor constraintEqualToAnchor:_statusBar.leftAnchor constant:10],
+        [_statusLabel.centerYAnchor constraintEqualToAnchor:_statusBar.centerYAnchor],
+        
+        [_metricsStack.topAnchor constraintEqualToAnchor:self.topAnchor constant:30],
+        [_metricsStack.bottomAnchor constraintEqualToAnchor:_statusBar.topAnchor constant:-15],
+        [_metricsStack.leftAnchor constraintEqualToAnchor:self.leftAnchor constant:20],
+        [_metricsStack.rightAnchor constraintEqualToAnchor:self.rightAnchor constant:-20]
+    ]];
 }
 
 - (void)didUpdateStatus:(int)status {
-	[_statusLabel setStringValue:[NSString stringWithUTF8String:STATUS_MESSAGES[status]]];
-	
-	switch (status) {
-		case BLUETOOTH_POWERED_ON_SCANNING:
-		case BIKE_DISCOVERED:
-			[_statusDot setColour: AMBER];
-			break;
-			
-		case BLUETOOTH_POWERED_OFF:
-			[_statusDot setColour: RED];
-			break;
-			
-		case BLUETOOTH_UNAUTHORISED:
-		case BLUETOOTH_UNSUPPORTED:
-		case BIKE_UNABLE_TO_CONNECT:
-		case BIKE_UNABLE_TO_DISCOVER:
-		case BIKE_ERROR:
-			[_statusDot stopFlashing];
-			[_statusDot setColour: RED];
-			break;
-			
-		case BIKE_CONNECTED:
-		case BIKE_NEEDS_CALIBRATION:
-			[_statusDot stopFlashing];
-			[_statusDot setColour: GREEN];
-			break;
-		
-		case BIKE_CONNECTED_RECEIVING:
-			[_statusDot startFlashing];
-			[_statusDot setColour: GREEN];
-			break;
-	}
+    [_statusLabel setStringValue:[NSString stringWithUTF8String:STATUS_MESSAGES[status]]];
+    
+    switch (status) {
+        case BLUETOOTH_POWERED_ON_SCANNING:
+        case BIKE_DISCOVERED:
+            [_statusDot setColour: AMBER];
+            break;
+            
+        case BLUETOOTH_POWERED_OFF:
+            [_statusDot setColour: RED];
+            break;
+            
+        case BLUETOOTH_UNAUTHORISED:
+        case BLUETOOTH_UNSUPPORTED:
+        case BIKE_UNABLE_TO_CONNECT:
+        case BIKE_UNABLE_TO_DISCOVER:
+        case BIKE_ERROR:
+            [_statusDot stopFlashing];
+            [_statusDot setColour: RED];
+            break;
+            
+        case BIKE_CONNECTED:
+        case BIKE_NEEDS_CALIBRATION:
+            [_statusDot stopFlashing];
+            [_statusDot setColour: GREEN];
+            break;
+            
+        case BIKE_CONNECTED_RECEIVING:
+            [_statusDot startFlashing];
+            [_statusDot setColour: GREEN];
+            break;
+    }
 }
 
 - (void)didReceiveData:(ICGLiveStreamData *)data
 {
-	[_cadenceLabel setStringValue: [NSString stringWithFormat:@"%d", data->cadence]];
-	[_resistanceLabel setStringValue: [NSString stringWithFormat:@"%d%%", data->brake_level]];
+    [_cadenceLabel setStringValue: [NSString stringWithFormat:@"%d", data->cadence]];
+    [_resistanceLabel setStringValue: [NSString stringWithFormat:@"%d%%", data->brake_level]];
 }
 
 @end
